5a941e4c83749eb1372d003745ea6a8396021bd7
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2021-12-19 20:58:36 -0600

    Colors plus opacity. Current results are chalky

--- js/editor/editor.js
+++ js/editor/editor.js
@@ -8,6 +8,8 @@ let currentTool;
 let currentColor = '#000000'
 let paper;
 
+let palette = {};
+
 const colorPicker = document.createElement('input');
 colorPicker.setAttribute('type', 'color');
 
@@ -22,19 +24,42 @@ const hiliteIcon = icon => {
   $(icon).addClass('active');
 }
 
+const opacityToHex = ( ) => {
+  let value = $('#opacity').val( );
+  value = ((value / 100) * 255) | 0;
+  return value.toString(16).padStart(2, '0');
+}
+
+const onOpacityChange = event => {
+  let val = opacityToHex( );
+  if(currentColor.length > 7)  {
+    currentColor = currentColor.substring(0, 7);
+  }
+  currentColor += val;
+  if(currentTool && currentTool.strokeStyle) currentTool.strokeStyle = currentColor;
+}
+
 const onColorClick = function(event) {
-  currentColor = $(event.target).css('background-color');
+  let index = $(event.target).index( );
+  currentColor = palette[$(event.target).index()] || "#000000";
   $('.gradient').css('background', `linear-gradient(-90deg, ${currentColor}, transparent)`);
+  currentColor += opacityToHex( );
   if(currentTool && currentTool.strokeStyle) currentTool.strokeStyle = currentColor;
 }
 
 const onColorDClick = function(event) {
-  pickColor( ).then(color => {$(event.target).css('background', color)})
+  pickColor( ).then(color => {
+    palette[$(event.target).index( )] = color;
+    $(event.target).css('background', color);
+    $(event.target).trigger('click');
+  })
 }
 
 const pickColor = ( ) => {
   return new Promise(function(resolve, reject) {
-    $(colorPicker).change(event => {resolve(event.target.value)});
+    $(colorPicker).change(event => {
+      resolve(event.target.value)}
+    );
     $(colorPicker).trigger('click', event.target);
   });
 }
@@ -48,6 +73,16 @@ const onIconClick = function(jqev) {
   setSize(currentTool.size);
 }
 
+const rgba2hex = color => {
+  if(color.match('rgba')) {
+    color = `#${color.match(/^rgba\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+  } else if(color.match('rgb')) {
+    color = `#${color.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+  } else {
+    color = '#000000';
+  }
+  return color;
+}
 
 const setSize = size => {
   currentTool.size = size;
@@ -64,6 +99,7 @@ const setListeners = ( ) => {
   $('#toolSizeInput').change(onToolSizeChange);
   $('.color').dblclick(onColorDClick);
   $('.color').click(onColorClick);
+  $('#opacity').change(onOpacityChange);
 }
 
 export default function( ) {
